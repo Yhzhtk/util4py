54f0543ac09f4dfd0dd996f6fd8768eb0bc3bae9
Daihui Gu <yhzhtk@gmail.com>
2014-02-14 13:28:33 +0800

    Create DRY.py

---
+++ use/DRY.py
@@ -0,0 +1,36 @@
+# Python 装饰器实现DRY(不重复代码)原则
+
+def increment(f):
+    def wrapped_f(args,a2):
+        c = f(args,a2)
+        c = int(c)
+        c += 3
+        print c
+        return c
+    return wrapped_f
+
+def increment1(f):
+    def wrapped_f(args,a2):
+        c = f(args,a2)
+        c = int(c)
+        c += 1
+        print c
+        return c
+    return wrapped_f
+
+def increment2(f):
+    def w(a1,a2):
+        print f(1,3)
+        return f(a1,a2) + 1
+    return w
+
+
+@increment1
+@increment
+@increment2
+def plus(a, b):
+    c = a + b
+    return c
+
+result = plus(4, 6)
+print result
