4e9225355182fd8d980f04bf3e56d1ee4f3e3cb0
Delfi-G <delfi@lagou.com>
2014-07-22 17:23:13 +0800

    添加注释

--- use/search/ddl.sql
+++ use/search/ddl.sql
@@ -1,23 +1,23 @@
 
-CREATE TABLE `key_statist` (   
-	`id` int(64) NOT NULL AUTO_INCREMENT,     
-	`host` varchar(24) DEFAULT NULL,          
-	`keyword` varchar(100) NOT NULL,          
-	`count` int(32) DEFAULT NULL,
-	`date` date DEFAULT NULL,    
-	PRIMARY KEY (`id`),          
-	KEY `KEYINDEX` (`keyword`)   
+CREATE TABLE `key_statist` (
+	`id` int(64) NOT NULL AUTO_INCREMENT COMMENT 'id,自增',
+	`host` varchar(24) DEFAULT NULL COMMENT '统计的主机',
+	`keyword` varchar(100) NOT NULL COMMENT '关键字',
+	`count` int(32) DEFAULT NULL COMMENT '搜索次数',
+	`date` date DEFAULT NULL COMMENT '统计日期',
+	PRIMARY KEY (`id`),
+	KEY `KEYINDEX` (`keyword`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
-CREATE TABLE `position_statist` (            
-	`id` int(64) NOT NULL AUTO_INCREMENT,      
-	`host` varchar(24) DEFAULT NULL,           
-	`pid` int(32) DEFAULT NULL,   
-	`adword` int(8) DEFAULT NULL, 
-	`count` int(32) DEFAULT NULL, 
-	`date` date DEFAULT NULL,     
-	PRIMARY KEY (`id`),           
-	KEY `PIDINDEX` (`pid`),       
-	KEY `ADWORDINDEX` (`adword`)  
+CREATE TABLE `position_statist` (
+	`id` int(64) NOT NULL AUTO_INCREMENT COMMENT 'id,自增',
+	`host` varchar(24) DEFAULT NULL COMMENT '统计的主机',
+	`pid` int(32) DEFAULT NULL COMMENT '职位ID',
+	`adword` int(8) DEFAULT NULL COMMENT '职位推广属性',
+	`count` int(32) DEFAULT NULL COMMENT '展示次数',
+	`date` date DEFAULT NULL COMMENT '统计日期',
+	PRIMARY KEY (`id`),
+	KEY `PIDINDEX` (`pid`),
+	KEY `ADWORDINDEX` (`adword`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 
