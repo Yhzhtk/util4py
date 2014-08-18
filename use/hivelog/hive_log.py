16e06f04d3fbd2b670f587edbbceb76f078ff98e
Delfi-G <delfi@lagou.com>
2014-08-18 17:15:49 +0800

    添加导出 sql

--- use/hivelog/hive_log.py
+++ use/hivelog/hive_log.py
@@ -12,20 +12,20 @@ base_day_sql = "select %s,count(*) from %s where log_date = %s and %s in (%%s) g
 base_all_sql = "select %s,count(*) from %s where %s in (%%s) group by %s"
 
 # search 表相关统计
-s_spc = [('search', 'spc', '搜索类型'), [('0', '联合搜索(0)'), ('1', '职位(1)'), ('2', '职位(2)'), ('3', '公司(3)'), ('4', '公司(4)'), ('5', '城市(5)'), ('', '无(空)')]]
-s_label = [('search', 'label_words', '搜索来源'), [('sug', 'Suggest词'), ('hot', '热门搜索词'), ('label', '左侧标签词'), ('label,label', '加筛选条件'), ('', '用户输入')]]
-s_salary = [('search', 'monthly_salary', '薪资点击'), [('50k以上', '50k以上'), ('2k-5k', '2k-5k'), ('25k-50k', '25k-50k'), ('5k-10k', '5k-10k'), ('10k-15k', '10k-15k'), ('15k-25k', '15k-25k'), ('', '无')]]
-s_experience = [('search', 'working_experience', '工作经验点击'), [('不限', '不限'), ('应届毕业生', '应届毕业生'), ('1年以下', '1年以下'), ('1-3年', '1-3年'), ('3-5年', '3-5年'), ('5-10年', '5-10年'), ('10年以上', '10年以上'), ('', '无')]]
-s_educational = [('search', 'educational_background', '教育背景点击'), [('不限', '不限'), ('大专', '大专'), ('本科', '本科'), ('硕士', '硕士'), ('博士', '博士'), ('', '无')]]
-s_nature = [('search', 'nature_of_work', '工作性质点击'), [('全职', '全职'), ('兼职', '兼职'), ('实习', '实习'), ('', '无')]]
-s_time = [('search', 'publish_time', '时间筛选点击'), [('今天', '今天'), ('3天内', '3天内'), ('一周内', '一周内'), ('一月内', '一月内'), ('', '无')]]
+s_spc = [('search', 'spc', '搜索类型', 's_spc'), [('0', '联合搜索(0)', 'spc0'), ('1', '职位(1)', 'spc1'), ('2', '职位(2)', 'spc2'), ('3', '公司(3)', 'spc3'), ('4', '公司(4)', 'spc4'), ('5', '城市(5)', 'spc5'), ('', '无(空)', 'empty')]]
+s_label = [('search', 'label_words', '搜索来源', 's_label'), [('sug', 'Suggest词', 'sug'), ('hot', '热门搜索词', 'hot'), ('label', '左侧标签词', 'label'), ('label,label', '加筛选条件', 'label_lable'), ('', '用户输入', 'empty')]] 
+s_salary = [('search', 'monthly_salary', '薪资点击', 's_salary'), [('2k以下', '2k以下', '_2'), ('50k以上', '50k以上', '50_'), ('2k-5k', '2k-5k', '2_5'), ('25k-50k', '25k-50k', '25_50'), ('5k-10k', '5k-10k', '5_10'), ('10k-15k', '10k-15k', '10_15'), ('15k-25k', '15k-25k', '15_25'), ('', '无', 'empty')]]
+s_experience = [('search', 'working_experience', '工作经验点击', 's_experience'), [('不限', '不限', 'any'), ('应届毕业生', '应届毕业生', '_0'), ('1年以下', '1年以下', '_1'), ('1-3年', '1-3年', '1_3'), ('3-5年', '3-5年', '3_5'), ('5-10年', '5-10年', '5_10'), ('10年以上', '10年以上', '10_'), ('', '无', 'empty')]]
+s_educational = [('search', 'educational_background', '教育背景点击', 's_educational'), [('不限', '不限', 'any'), ('大专', '大专', 'zhuan'), ('本科', '本科', 'ben'), ('硕士', '硕士', 'shuo'), ('博士', '博士', 'bo'), ('', '无', 'empty')]]
+s_nature = [('search', 'nature_of_work', '工作性质点击', 's_nature'), [('全职', '全职', 'quan'), ('兼职', '兼职', 'jian'), ('实习', '实习', 'shi'), ('', '无', 'empty')]]
+s_time = [('search', 'publish_time', '时间筛选点击', 's_time'), [('今天', '今天', '_0'), ('3天内', '3天内', '_3'), ('一周内', '一周内', '_7'), ('一月内', '一月内', '_30'), ('', '无', 'empty')]]
 # 页码自动生成0到30页
-s_pn = [('search', 'pn', '翻页统计'), []]
-s_pn[1].extend([(str(i), '第%d页' % i) for i in range(31)])
-s_pn[1].append(('', '无'))
+s_pn = [('search', 'pn', '翻页统计', 's_pn'), []]
+s_pn[1].extend([(str(i), '第%d页' % i, str(i)) for i in range(31)])
+s_pn[1].append(('', '无', 'empty'))
 
 # position 表相关统计
-p_source = [('view_position', 'current_source', '职位查看来源'), [('profile_rec', '个人简历页'), ('home_hot', '首页热门'), ('home_latest', '首页最新'), ('home_rec', '首页推荐'), ('rec', '推荐页'), ('company_list', '公司列表页'), ('job_rec', '猜你喜欢'), ('pl', '公司职位列表'), ('position_rec', '相似推荐'), ('company', '公司页'), ('', '无'), ('search', '搜索页')]]
+p_source = [('view_position', 'current_source', '职位查看来源', 'p_source'), [('profile_rec', '个人简历页', 'profile_rec'), ('home_hot', '首页热门', 'home_hot'), ('home_latest', '首页最新', 'home_latest'), ('home_rec', '首页推荐', 'home_rec'), ('rec', '推荐页', 'rec'), ('company_list', '公司列表页', 'company_list'), ('job_rec', '猜你喜欢', 'job_rec'), ('pl', '公司职位列表', 'pl'), ('position_rec', '相似推荐', 'position_rec'), ('company', '公司页', 'company'), ('', '无', 'empty'), ('search', '搜索页', 'search')]]
 
 def generate_sql(words, log_date):
     tinfo = words[0]
@@ -39,6 +39,14 @@ def generate_sql(words, log_date):
     sql = sql %  ','.join(["'%s'" % word[0] for word in params])
     return sql
 
+def export_sql(para, values):
+    sql = "insert into %s (%s) values (%s)"
+    table_name = para[0][3]
+    fields = ','.join([ v[2] for v in para[1]])
+    values = ','.join(values)
+    sql = sql % (table_name, fields, values)
+    return sql
+    
 def execute_hive(para, log_date, to_file):
     sql = generate_sql(para, log_date)
     hive_sql = '''hive -e "%s"''' % sql
@@ -76,6 +84,8 @@ def execute_hive(para, log_date, to_file):
     rstr += '\t'.join(head) + "\n"
     rstr +=  '\t'.join(value) + "\n"
     rstr +=  '\t'.join(ratio) + "\n"
+    rstr +=  '\n'
+    rstr += "insert into %s values (%s)" % (para.__name__, ",".join(value))
     rstr +=  '\n'
     rstr +=  '\n'
     print rstr
